6ff81187a2a4d6d1a80f2c39744a67be14e5436a
emrah <emrah.com@gmail.com>
2017-10-05 13:04:29 +0300

    convert to PyPI module

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='mydaemon',
+    version='0.1.7',
+    packages=['mydaemon'],
+    description='Python daemon module',
+    url = 'https://github.com/emrahcom/MyDaemon',
+    author = 'emrah',
+    author_email = 'emrah.com@gmail.com',
+    license='MIT',
+    keywords = 'daemon service background',
+    python_requires='>=3',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3'
+    ]
+)
